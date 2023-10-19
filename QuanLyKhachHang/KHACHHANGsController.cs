b41e5c2442acdc80135533f11306b101321cea40
Tuan Anh <nntuananh3011@gmail.com>
2023-10-19 19:17:58 +0700

    Quản Lý Khách Hàng
    git commit -m "KAN-24 <Quản lý khách hàng>"

---
+++ QuanLyKhachHang/KHACHHANGsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Admin.Models;
+
+namespace Admin.Controllers
+{
+    public class KHACHHANGsController : Controller
+    {
+        private DAPMEntities db = new DAPMEntities();
+
+        // GET: KHACHHANGs
+        public ActionResult Index()
+        {
+            return View(db.KHACHHANG.ToList());
+        }
+
+        // GET: KHACHHANGs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KHACHHANG kHACHHANG = db.KHACHHANG.Find(id);
+            if (kHACHHANG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kHACHHANG);
+        }
+
+        // GET: KHACHHANGs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KHACHHANGs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaKH,HoTenKH,DiaChiKH,DienThoaiKH,TenDN,Matkhau,Ngaysinh,Gioitinh,Email,Daduyet")] KHACHHANG kHACHHANG)
+        {
+            if (ModelState.IsValid)
+            {
+                db.KHACHHANG.Add(kHACHHANG);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kHACHHANG);
+        }
+
+        // GET: KHACHHANGs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KHACHHANG kHACHHANG = db.KHACHHANG.Find(id);
+            if (kHACHHANG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kHACHHANG);
+        }
+
+        // POST: KHACHHANGs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaKH,HoTenKH,DiaChiKH,DienThoaiKH,TenDN,Matkhau,Ngaysinh,Gioitinh,Email,Daduyet")] KHACHHANG kHACHHANG)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kHACHHANG).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kHACHHANG);
+        }
+
+        // GET: KHACHHANGs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KHACHHANG kHACHHANG = db.KHACHHANG.Find(id);
+            if (kHACHHANG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kHACHHANG);
+        }
+
+        // POST: KHACHHANGs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            KHACHHANG kHACHHANG = db.KHACHHANG.Find(id);
+            db.KHACHHANG.Remove(kHACHHANG);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
